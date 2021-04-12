091ed5a2b15a2148de45bcc3fb70fdb175c170e9
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-04-12 14:45:27 -0400

    Is able to isolate the story URLS

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 const testing = document.querySelector(".testing-js");
+//declare an empty array to fill with story id's
+
 //the following will
 fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
     // Now process the raw response into an object
     .then((res) => res.json())
     // Now process the above JSON, we will first console log a message
-    //but we want to use this space to change the img src
     .then((data) => {
-        console.log('success!');
+        console.log('Data is: ');
         console.log(data);
-        console.log("array length is: " + data.length)
-        console.log(data[0]);
-    }
-    /*.then(data) =>{
-
-    }*/)
+        return data;
+    })
+    .then((storyIDs) => {
+        for(let i = 0; i < 5; i ++){
+            fetch(`https://hacker-news.firebaseio.com/v0/item/${storyIDs[i]}.json?print=pretty`)
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log("New data is: ");
+                    console.log(data.url);
+                })
+        }
+       
+    })
