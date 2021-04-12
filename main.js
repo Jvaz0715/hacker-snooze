8dcee8786e333e80a8f1324b835b5a4122cf8846
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-04-12 16:01:58 -0400

    Converts unix time to date, time format

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
     .then((res) => res.json())
     // Now process the above JSON, we will first console log a message
     .then((data) => {
-        console.log('Data is: ');
-        console.log(data);
+        //console.log('Data is: ');
+        //console.log(data);
         return data;
     })
     .then((storyIDs) => {
@@ -16,13 +16,26 @@ fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
             fetch(`https://hacker-news.firebaseio.com/v0/item/${storyIDs[i]}.json?print=pretty`)
                 .then((res) => res.json())
                 .then((data) => {
-                    console.log("New data is: ");
-                    console.log("story title is: " + data.title);
-                    console.log("story url is: " + data.url);
+                    //console.log("New data is: ");
+                    //console.log("story title is: " + data.title);
+                    //console.log("story url is: " + data.url);
                     const newLink = document.createElement("a");
+                    const storyDate = document.createElement("p");
+                    
+
+                    const unixTimestamp = data.time;
+
+                    const milliseconds = data.time * 1000 // 1575909015000
+
+                    const dateObject = new Date(milliseconds)
+
+                    const humanDateFormat = dateObject.toLocaleString() //2019-12-9 10:30:15
+                    storyDate.innerText = humanDateFormat;
+
                     newLink.target = "_blank";
                     newLink.href = data.url;
                     newLink.innerText = data.title;
+                    newLink.appendChild(storyDate);
                     testing.appendChild(newLink);
                 })
         }
