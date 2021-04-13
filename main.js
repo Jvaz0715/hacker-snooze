716067f4768647de6a3e32181fa3218c3a366bcb
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-04-13 12:54:15 -0400

    adds more styling to stories layout

--- main.js
+++ main.js
@@ -12,14 +12,41 @@ fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
         return data;
     })
     .then((storyIDs) => {
-        for(let i = 0; i < 100; i ++){
+        for(let i = 0; i < 5; i ++){
             fetch(`https://hacker-news.firebaseio.com/v0/item/${storyIDs[i]}.json?print=pretty`)
                 .then((res) => res.json())
                 .then((data) => {
-                    //console.log("New data is: ");
-                    //console.log("story title is: " + data.title);
-                    //console.log("story url is: " + data.url);
-                    const newLink = document.createElement("a");
+                    /*<div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="https://images.unsplash.com/photo-1546422904-90eab23c3d7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2104&q=80" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>*/
+                    // creates the div that will hold the information
+                    const storyDiv = document.createElement("div");
+                    //now add the bootstrap classes necessary to the story div
+                    storyDiv.classList.add("card");
+                    //now sets the width of the story card
+                    storyDiv.style.width = "18rem";
+                    //now create img within storyDiv
+                    const storyIMG = document.createElement("img");
+                    storyIMG.src = "https://images.unsplash.com/photo-1546422904-90eab23c3d7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2104&q=80";
+                    storyIMG.classList.add("card-img-top")
+                    //now append the img to the storyDiv
+                    storyDiv.appendChild(storyIMG);
+
+                    //now create the div that will hold the cardbody
+                    const cardBody = document.createElement("div");
+                    // add the card-img-top class to the cardBody
+                    cardBody.classList.add("card-body");
+
+                    //for testing, append the storyDIV to the container
+                    storiesContainer.appendChild(storyDiv)
+                    //for testing, append the cardBody to the container
+                    storyDiv.appendChild(cardBody);
+
+
+                    /*const newLink = document.createElement("a");
                     const storyDate = document.createElement("p");
                     
 
@@ -36,7 +63,7 @@ fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
                     newLink.href = data.url;
                     newLink.innerText = data.title;
                     newLink.appendChild(storyDate);
-                    storiesContainer.appendChild(newLink);
+                    storiesContainer.appendChild(newLink);*/
                 })
         }
        
